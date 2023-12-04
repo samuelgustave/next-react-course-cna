9d7f1c9c731cc46cd6372358eed1ef9eb4673e95
Samuel Gustave <gustavesam@gmail.com>
2023-12-04 18:15:53 -0500

    Premier commit

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.DATABASE_URL!);
+
+type Quiz = {
+  quiz_id: number;
+  title: string;
+};
+
+async function Quizzes() {
+  const quizzes: Quiz[] = await sql`
+    SELECT * FROM quizzes
+  `;
+
+  return (
+    <ul>
+      {quizzes.map(quiz => (
+        <li key={quiz.quiz_id}>
+          <Link href={`/quiz/${quiz.quiz_id}`}>{quiz.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function Home() {
+  return (
+    <section>
+      <h1>All Quizzes</h1>
+      <Quizzes />
+    </section>
+  );
+}
